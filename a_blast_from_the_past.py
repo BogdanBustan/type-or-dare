8d3c7bc13476350566b55f5f66ac00553682cee1
Bogdan Bustan <bustan.bogdan.ioan@gmail.com>
2024-10-14 21:08:25 +0300

    adds examples for typing collections before typing

--- a_blast_from_the_past.py
+++ a_blast_from_the_past.py
@@ -1,3 +1,5 @@
+# from typing import List
+
 def greet(name):  # type: (str) -> str
     return 'Hello, ' + name
 
@@ -17,4 +19,13 @@ def midpoint(p1, p2):  # type: (Point, Point) -> Point
     """Calculate the midpoint between two points."""
     return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
 
-greet(1)
+
+# def process_names(names):  # type: (List[str]) -> List[str]
+#     result = []
+#     for name in names:
+#         result.append(name.upper())
+#     return result
+
+
+print(greet(1))
+# print(process_names(["abc", "def"]))
